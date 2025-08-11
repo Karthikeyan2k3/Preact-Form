c6f1bd5dd43673704ab84c24287156b23ea9e123
Karthikeyan B <dineshkarthi2015@gmail.com>
2025-08-11 18:16:40 +0530

    Format form component

--- src/Components/SignalsForm.jsx
+++ src/Components/SignalsForm.jsx
@@ -31,27 +31,63 @@ function SignalsForm(){
     return(
         <form onSubmit={displayName}>
             <h1>Basic Form</h1>
-            <label />Firstname : <input name="firstname" value={firstName} placeholder="Enter Firstname" onInput={e=>(firstName.value=e.currentTarget.value)}/><br/>
-            <label />Lastname  : <input name="lastname" value={lastName} placeholder="Enter Lastname" onInput={e=>(lastName.value=e.currentTarget.value)}/><br/>
-            <label />Allow Contact : <input type="checkbox" onClick={toggleContact}/><br/>
-            <label />Phone <input type="radio" value={"Phone"} name={"contact"} onClick={e=>contactMethod.value = e.currentTarget.value } disabled={!allowContact.value}/>
-            <label />E-Mail <input type="radio" value={"Mail"} name={"contact"} onClick={e=>contactMethod.value = e.currentTarget.value } disabled={!allowContact.value}/><br/>
-            <label />Location : 
-            <select value={selectValue.value} onChange={e => selectValue.value=e.currentTarget.value}>
-                <option value="" disabled>Select</option> 
-				<option value="India">India</option>
-				<option value="China">China</option>
-				<option value="Russia">Russia</option>
-			</select><br/>
+            <label>Firstname :
+                <input 
+                    name="firstname" 
+                    value={firstName} 
+                    placeholder="Enter Firstname" 
+                    onInput={e=>(firstName.value=e.currentTarget.value)}
+                /><br/>
+            </label> 
+            <label>Lastname: 
+                <input 
+                    name="lastname" 
+                    value={lastName} 
+                    placeholder="Enter Lastname" 
+                    onInput={e=>(lastName.value=e.currentTarget.value)}
+                /><br/>
+            </label>
+            <label>Allow Contact : 
+                <input 
+                    type="checkbox" 
+                    onClick={toggleContact}
+                /><br/>
+            </label>
+            <label>Phone 
+                <input 
+                    type="radio" 
+                    value={"Phone"} 
+                    name={"contact"} 
+                    onClick={e=>contactMethod.value = e.currentTarget.value } 
+                    disabled={!allowContact.value}
+                />
+            </label>
+            <label>E-Mail 
+                <input 
+                    type="radio" 
+                    value={"Mail"} 
+                    name={"contact"} 
+                    onClick={e=>contactMethod.value = e.currentTarget.value } 
+                    disabled={!allowContact.value}
+                /><br/>
+            </label>
+            <label>Location :
+                <select value={selectValue.value} onChange={e => selectValue.value=e.currentTarget.value}>
+                    <option value="" disabled>Select</option> 
+                    <option value="India">India</option>
+                    <option value="China">China</option>
+                    <option value="Russia">Russia</option>
+                </select><br/>
+            </label> 
             <button type={"submit"}>Submit</button>
-            {showDetails.value && (
-                <>
-                    <h2>Form-Details</h2>
-                    <p>Full Name : {fullName.value}</p>
-                    <p>Contact Info : {contactInfo.value}</p>
-                    <p>Location : {selectValue.value}</p>
-                </>
-            )}
+                {showDetails.value && (
+                    <>
+                        <h2>Form-Details</h2>
+                        <p>Full Name : {fullName.value}</p>
+                        <p>Contact Info : {contactInfo.value}</p>
+                        <p>Location : {selectValue.value}</p>
+                    </>
+                )}
         </form>
     );
 }
